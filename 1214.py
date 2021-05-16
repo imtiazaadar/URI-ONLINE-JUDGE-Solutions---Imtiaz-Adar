e3952e12b064e4821bd939559ef5868c75cb7ebb
imtiazaadar <75721079+imtiazaadar@users.noreply.github.com>
2021-05-16 17:59:52 +0600

    Add files via upload

--- 1214.py
+++ 1214.py
@@ -1,5 +1,27 @@
-x=int(input())
-for i in range (x):
-    val=list(map(int,input().split()))
-    for j in range(val):
-        val1=list(map(int,input().split()))
+#URI - Above The Average 1022
+# Imtiaz Adar
+
+#percentage of students whose grade is above average, rounded to 3 decimal places.
+
+while True:
+    try:
+        x=int(input()) #size
+        sum=0
+        above=0
+        arr=[]
+        media=0
+        for i in range(x):
+            y=input().split() #took input of the whole line as a string
+            val=int(y.pop(0)) #then poped the first value as the size of that string and stored into 'val'
+            for j in y:
+                sum+=int(j) #sum
+                arr.append(j) #appended in list
+            media=float(sum/val) #calculated average
+            for k in range(val):
+                if int(arr[k])>media:
+                    above+=1 #counting by 1 if any element is greater than the average value
+            print(f'{((above*100.0)/val):.3f}%')
+            above,sum=0,0
+            arr=[]
+    except EOFError:
+        break
